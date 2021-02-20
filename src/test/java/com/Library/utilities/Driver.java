014701e8f9ff0f6b035699bd1f2cb95162c801ae
Baris312 <barmagan81@gmail.com>
2021-02-20 08:42:17 -0600

    Step1 : Create project

---
+++ src/test/java/com/Library/utilities/Driver.java
@@ -0,0 +1,69 @@
+package com.Library.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import javax.security.auth.login.Configuration;
+import java.util.concurrent.TimeUnit;
+
+public class Driver
+    {
+
+        private Driver()
+            {
+            }
+
+
+        private static WebDriver driver;
+
+
+        public static WebDriver getDriver()
+            {
+                if (driver == null)
+                    {
+                        String browser = ConfigurationReader.getProperty("brpwser");
+                        switch (browser)
+                            {
+                                case "chrome":
+                                    WebDriverManager.chromedriver().setup();
+                                    driver = new ChromeDriver();
+                                    driver.manage().window().maximize();
+                                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                                    break;
+                                case "firefox":
+                                    WebDriverManager.firefoxdriver().setup();
+                                    driver = new FirefoxDriver();
+                                    driver.manage().window().maximize();
+                                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                                    break;
+                                default:
+                                    WebDriverManager.chromedriver().setup();
+                                    driver = new ChromeDriver();
+                                    driver.manage().window().maximize();
+                                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                                    break;
+
+                            }
+                    }
+                return driver;
+
+            }
+
+
+
+        public static void closeDriver()
+        {
+            if (driver != null)
+            {
+                driver.quit();
+                driver = null;
+            }
+
+
+        }
+
+
+
+    }
